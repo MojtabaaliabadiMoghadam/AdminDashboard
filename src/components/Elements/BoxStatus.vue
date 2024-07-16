1df2d6d08cbe59f344584c53c10ab67faa829017
Sadr IT <mojiland2002@gmail.com>
2024-07-16 14:34:10 +0330

    this is saet data in dashboard

---
+++ src/components/Elements/BoxStatus.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col bg-wh">
+
+  </div>
+</template>
+<script setup>
+const props = defineProps()
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="h-full w-full bg-[#EDEEF3]">
-    main
+    <div class="flex flex-col p-8 gap-4">
+      <span class="font-bold text-[26px]">Dashboard</span>
+      <div class="grid grid-cols-4">
+        <BoxStatus />
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+import BoxStatus from '@/components/Elements/BoxStatus.vue'
+</script>
