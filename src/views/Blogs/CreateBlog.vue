959d3880c956bd508c9cbde032799484599119b0
MojtabaaliabadiMoghadam <mojiland2002@gmail.com>
2024-08-26 22:41:21 +0330

    add input and select box

--- src/views/Blogs/CreateBlog.vue
+++ src/views/Blogs/CreateBlog.vue
@@ -1,3 +1,11 @@
 <template>
-  this is a test
+  <layout-of-pages>
+    <div class="!h-[90%]">
+      <ckeditor-blog/>
+    </div>
+  </layout-of-pages>
 </template>
+<script setup>
+import LayoutOfPages from "@/components/Elements/LayoutOfPages.vue";
+import CkeditorBlog from "@/components/Articles/CkeditorBlog.vue";
+</script>
